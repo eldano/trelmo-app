a04e83b3c1a6444a84907f1a46ce484b96c77da8
Daniel Gomez de Souza <daniel@wyeworks.com>
2017-06-19 17:14:22 -0300

    refactor main.elm

--- main.elm
+++ main.elm
@@ -3,62 +3,63 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 
 main = Html.beginnerProgram {
-  model = model,
+  model = list1,
   view = view,
   update = update
   }
 
-type alias Model = TList
 type alias TList = { name : String, newCard : String, cards : List TCard }
 type alias TCard = { description : String }
 
 type Msg = Submit | Name String
 
-update : Msg -> Model -> Model
-update msg model =
+update : Msg -> TList -> TList
+update msg list =
   case msg of
     Name name ->
-      { model | newCard = name }
+      { list | newCard = name }
     Submit ->
-      let newCards = List.append model.cards [TCard model.newCard]
+      let newCards = List.append list.cards [TCard list.newCard]
       in
-        { model | cards = newCards, newCard = "" }
+        { list | cards = newCards, newCard = "" }
 
 -- PLAYGROUND
 
 card1 = TCard "Cartita"
 card2 = TCard "Cartonga"
 
-model : Model
-model = TList "Listardi" "" [card1, card2]
+list1 : TList
+list1 = TList "Listardi" "" [card1, card2]
 
 -- VIEW
 
-view model =
+view list =
   div [ class "panel panel-default" ] [
-    displayTitle model
-  , displayCards model
-  , displayForm model
+    displayTitle list
+  , displayCards list
+  , displayForm list
   ]
 
 displayTitle: TList -> Html Msg
 displayTitle list =
-  div [ class "panel-body" ] [ h3 [ class "panel-title" ] [ text model.name ]]
+  div [ class "panel-body" ] [
+    h3 [ class "panel-title" ] [ text list.name ]
+  ]
 
 displayCard : TCard -> Html Msg
 displayCard card =
   li [ class "list-group-item" ] [ text card.description ]
 
-displayCards : Model -> Html Msg
+displayCards : TList -> Html Msg
 displayCards list =
   ul [class "list-group"] (List.map displayCard list.cards)
 
-displayForm : Model -> Html Msg
-displayForm model =
+displayForm : TList -> Html Msg
+displayForm list =
   div [ class "panel-footer" ] [
     div [ class "form-group" ] [
       textarea [ onInput Name,
-                 value model.newCard,
+                 value list.newCard,
                  placeholder "card name",
                  class "form-control" ] []
     ]
