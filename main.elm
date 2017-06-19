0ac9f728684edddaa9d1c2a684c3dff3623c6b39
Daniel Gomez de Souza <daniel@wyeworks.com>
2017-06-19 17:02:55 -0300

    Refactor main

--- main.elm
+++ main.elm
@@ -9,7 +9,7 @@ main = Html.beginnerProgram {
   }
 
 type alias Model = TList
-type alias TList = { name : String, newCard: String, cards : List TCard }
+type alias TList = { name : String, newCard : String, cards : List TCard }
 type alias TCard = { description : String }
 
 type Msg = Submit | Name String
@@ -34,44 +34,33 @@ model = TList "Listardi" "" [card1, card2]
 
 -- VIEW
 
---<div class="panel-footer">
---  <form action="#" method="post">
---    <div class="form-group">
---      <textarea class="form-control" placeholder="Add a new card" name="title"></textarea>
---    </div>
---    <input type="submit" name="commit" value="Create Card" class="btn btn-default btn-sm"/>
---  </form>
---</div>
-
-
 view model =
   div [ class "panel panel-default" ] [
-    div [ class "panel-body" ] [
-      h3 [ class "panel-title" ] [ text model.name ]
-    ]
-  , ul [ class "list-group" ] (displayCards model)
-  , div [ class "panel-footer" ] [
-      div [ class "form-group" ] [textarea [onInput Name,
-                                            value model.newCard,
-                                            placeholder "card name",
-                                            class "form-control" ] [] ]
-    , button [ onClick Submit, class "btn btn-default btn-sm" ] [ text "Create Card" ]
-    ]
+    displayTitle model
+  , displayCards model
+  , displayForm model
   ]
+
+displayTitle: TList -> Html Msg
+displayTitle list =
+  div [ class "panel-body" ] [ h3 [ class "panel-title" ] [ text model.name ]]
 
 displayCard : TCard -> Html Msg
 displayCard card =
   li [ class "list-group-item" ] [ text card.description ]
 
-displayCards : Model -> List (Html Msg)
+displayCards : Model -> Html Msg
 displayCards list =
-  List.map displayCard list.cards
+  ul [class "list-group"] (List.map displayCard list.cards)
 
-displayForm : Model -> List (Html Msg)
+displayForm : Model -> Html Msg
 displayForm model =
-  [
-    div [] [textarea [onInput Name,
-                      value model.newCard,
-                      placeholder "card name"] [] ]
-  , div [] [ button [ onClick Submit ] [ text "Submit" ] ]
+  div [ class "panel-footer" ] [
+    div [ class "form-group" ] [
+      textarea [ onInput Name,
+                 value model.newCard,
+                 placeholder "card name",
+                 class "form-control" ] []
+    ]
+  , button [ onClick Submit, class "btn btn-default btn-sm" ] [ text "Create Card" ]
   ]
